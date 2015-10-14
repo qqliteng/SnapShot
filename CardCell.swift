2b5181981389dcc063c5306fb6d4f12c38c8121e
jacobTough <qqliteng@163.com>
2015-10-14 17:43:47 +0800

    card cell Development

---
+++ CardCell.swift
@@ -0,0 +1,41 @@
+//
+//  CardCell.swift
+//  SnapShot
+//
+//  Created by Jacob Li on 12/10/2015.
+//  Copyright © 2015 Jacob Li. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CardCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var displayImage: UIImageView!
+    
+    @IBOutlet weak var photographerIDLabel: UILabel!
+    
+    @IBOutlet weak var photographerPriceLabel: UILabel!
+    
+    
+    @IBOutlet weak var servicesCataLabel: UILabel!
+    
+    
+    @IBAction func appointButton(sender: AnyObject) {
+    }
+    
+   
+    @IBAction func markButton(sender: AnyObject) {
+    }
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
--- FrontViewController.swift
+++ FrontViewController.swift
@@ -10,32 +10,14 @@ import UIKit
 import Alamofire
 import SwiftyJSON
 
-class FrontViewController: UIViewController {
+class FrontViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var mainTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        let url = "http://api.openweathermap.org/data/2.5/forecast"
-        let params = ["lat":39.0, "lon":41.0]
-        print(params)
-        
-        Alamofire.request(.GET, url, parameters: params)
-            .responseJSON { response in
-                if(response.result.error != nil) {
-                    print("Error: \(response.description)")
-                    print(response.request)
-                    print(response.response)
-                    
-                }
-                else {
-                    print("Success: \(url)")
-                    print(response.request)
-                    let json = JSON(response.result.value!)
-                    print(json)
-                }
-        }
-        
+        let nib = UINib(nibName: "CardCell", bundle: nil)
+        mainTableView.registerNib(nib, forCellReuseIdentifier: "cardCell")
         
     }
 
@@ -44,6 +26,24 @@ class FrontViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell:UITableViewCell
+        
+        if indexPath.section == 0 && indexPath.row == 0 {
+            cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: nil)
+            cell.backgroundColor = UIColor.clearColor()
+            cell.contentView.backgroundColor = UIColor.clearColor()
+            cell.clipsToBounds = true
+            
+            var slideRect = CGRect(origin: CGPoint(x: 0, y: 0), size: CGSize(width: tableView.frame.width, height: CGFloat(IMAGE_HEIGHT)))
+            
+            var slideView = 
+        }
+    }
 
 }
 
